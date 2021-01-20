80162841dc06ea87607ef1388c2f0039ff1d9529
Shady Hashoul <shady.h@clewmed.com>
2021-01-20 23:02:03 +0200

    The Content class

--- ssg/content.py
+++ ssg/content.py
@@ -26,7 +26,7 @@ class Content(Mapping):
 
     @property
     def type(self):
-        return self.data["type"] if not is None else None
+        return self.data["type"] if "type" in self.data else None
 
 
     @setattr
